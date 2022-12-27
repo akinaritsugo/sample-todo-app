5ce83b0beb283cec724f66ba031b027e8dead232
Akinari Tsugo <akinaritsugo@microsoft.com>
2022-12-27 16:55:21 +0900

    シンプルなテキストHTMLのみに戻し

--- web/src/App.js
+++ web/src/App.js
@@ -1,41 +1,6 @@
-import CreateTask from './tasks/CreateTask';
-import ListTasks from './tasks/ListTasks';
 import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-async function loadTasks() {
-  try{
-    var res = await axios.get("/api/GetTasks");
-    return res.data;
-  }catch(err){
-    console.log(err.message);
-    throw err;
-  }
-};
-
-async function saveTask() {
-
-};
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      var res = await loadTasks();
-      setTasks(res);
-    })();
-  }, [])
-
-  var onAdd_task = function (task) {
-    setTasks([...tasks, task]);
-  };
-
-  var onDelete_task = function (task) {
-    setTasks(tasks.filter((item) => item.id !== task.id));
-  };
-
   return (
     <div className="container">
       <header className='my-4'>
@@ -43,20 +8,10 @@ function App() {
       </header>
       <main>
         <div>
-          <CreateTask
-            onAdd={(task) => onAdd_task(task)}
-          />
         </div>
         <div>
-          <ListTasks
-            tasks={tasks}
-            onDelete={(task) => onDelete_task(task)}
-          />
         </div>
       </main>
-      <footer>
-
-      </footer>
     </div>
   );
 }
